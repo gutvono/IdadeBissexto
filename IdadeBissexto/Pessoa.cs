064da00919f410013e5fca76acb9bcef884e9a2a
Gustavo <guvono4@gmail.com>
2024-05-05 23:18:58 -0300

    Add project files.

---
+++ IdadeBissexto/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿namespace IdadeBissexto
+{
+    internal class Pessoa
+    {
+        string nome;
+        int idade;
+        string profissao;
+
+        public Pessoa(string n, int i, string p)
+        {
+            this.Nome = n;
+            this.Idade = i;
+            this.Profissao = p;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Idade { get => idade; set => idade = value; }
+        public string Profissao { get => profissao; set => profissao = value; }
+
+        public int GetIdadeBissexto()
+        {
+            int idadeBissexto = 0;
+            int anoAtual = DateTime.Now.Year;
+            for (int ano = anoAtual - this.idade; ano <= anoAtual; ano++)
+            {
+                if (ano % 4 == 0) idadeBissexto++;
+            }
+            return idadeBissexto;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Seu nome é {this.nome} e sua profissão é {this.profissao} e tem {this.idade} anos.\n" +
+                $"Sua idade em anos bissextos {this.GetIdadeBissexto()}.");
+        }
+    }
+}
---
+++ IdadeBissexto/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace IdadeBissexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nInforme seu nome:");
+            string nome = Console.ReadLine();
+
+            Console.Write("\nInforme sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInforme sua profissão: ");
+            string profissao = Console.ReadLine();
+
+            Pessoa pessoa = new(nome, idade, profissao);
+            pessoa.Print();
+        }
+    }
+}
